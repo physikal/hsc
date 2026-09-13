9886e0ccd1dd254b05af33c50167e3ef21d933f2
physikal <boody@physikal.com>
2026-09-12 18:22:52 -0700

    chore: add source files (chunk 14)

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+const usd = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+});
+
+export function formatUsd(amount: number) {
+  return usd.format(amount);
+}
